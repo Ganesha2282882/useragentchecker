fc31a6e5f7bbc9ef0b3cc164aed22fea4f938c37
Ganesha Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2020-05-27 09:27:52 -0400

    Add files via upload

---
+++ ua.js
@@ -0,0 +1,4 @@
+var a = "<h4>Your user agent string is&nbsp<pre>" + navigator.userAgent + "</pre></h4>";
+document.getElementById("ua").innerHTML = a;
+var b = "<h4>Your platform is&nbsp<pre>" + navigator.platform + "</pre></h4>";
+document.getElementById("pl").innerHTML = b;
